846d8047823148adb323ccd5e4bd1ba8fcfb6360
ananyasrii <anniethenun@gmail.com>
2025-09-25 18:25:55 +0530

    version2

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const mockPlaylists = {
     happy: [
-      { title: "Happy Song 1", artist: "Artist A", cover: "https://via.placeholder.com/180?text=Happy+1" },
-      { title: "Happy Song 2", artist: "Artist B", cover: "https://via.placeholder.com/180?text=Happy+2" }
+      { title: "Happy", artist: "Pharell Williams", cover: "https://upload.wikimedia.org/wikipedia/en/2/23/Pharrell_Williams_-_Happy.jpg" },
+      { title: "Good Vibrations", artist: "The Beach Boys", cover: "https://www.udiscovermusic.com/wp-content/uploads/2016/10/The-Beach-Boys-Good-Vibrations.jpg" },
+      { title: "September", artist: "Earth, Wind & Fire", cover: "https://upload.wikimedia.org/wikipedia/en/1/18/EarthWindAndFireSeptember7InchSingleCover.jpg" },
+      { title: "Uptown Funk", artist: "Mark Ronson ft. Bruno Mars", cover: "https://i.scdn.co/image/ab67616d0000b273e419ccba0baa8bd3f3d7abf2" },
+      { title: "Walking on Sunshine", artist: "Katrina & The Waves", cover: "https://m.media-amazon.com/images/I/81KZ8aCX6vL._UF894,1000_QL80_.jpg" },
+      { title: "Good Life", artist: "OneRepublic", cover: "https://upload.wikimedia.org/wikipedia/en/4/4e/OneRepublic_Good_Life.jpg" },
+      { title: "Shut Up and Dance", artist: "WALK THE MOON", cover: "https://i0.wp.com/classicsongoftheday.com/wp-content/uploads/2023/04/Screenshot-2023-04-15-133755.png?fit=623%2C618&ssl=1" },
+      { title: "Can't Stop the Feeling!", artist: "Justin Timberlake", cover: "https://i1.sndcdn.com/artworks-Zx3ZZMU1U2vp7jRk-IZaxqg-t500x500.jpg" },
+      
     ],
     chill: [
-      { title: "Chill Song 1", artist: "Artist C", cover: "https://via.placeholder.com/180?text=Chill+1" },
-      { title: "Chill Song 2", artist: "Artist D", cover: "https://via.placeholder.com/180?text=Chill+2" }
+      { title: "Lost in Japan", artist: "Shawn Mendes", cover: "https://upload.wikimedia.org/wikipedia/en/4/42/Shawn_Mendes_Lost_in_Japan.png" },
+      { title: "Sunflower", artist: "Post Malone, Swae Lee", cover: "https://i.scdn.co/image/ab67616d0000b273e2e352d89826aef6dbd5ff8f" },
+       { title: "Sunday Best", artist: "Surfaces", cover: "https://upload.wikimedia.org/wikipedia/en/7/77/Surfaces_-_Sunday_Best_%28Live%29.png" },
+      { title: "Better Now", artist: "Post Malone", cover: "https://i.scdn.co/image/ab67616d0000b273b1c4b76e23414c9f20242268" },
+      { title: "Good Day", artist: "Greg Street, Nappy Roots", cover: "https://i.scdn.co/image/ab67616d0000b273f9fde61817e49769b3d89131" },
+       { title: "Electric Feel", artist: "MGMT", cover: "https://i1.sndcdn.com/artworks-dcVQ6twCHNl9a5ML-mSZ6oQ-t500x500.png" },
+      { title: "Location", artist: "Khalid", cover: "https://upload.wikimedia.org/wikipedia/en/3/38/LocationKhalid.png" },
+       { title: "Put It All On Me", artist: "Ed Sheeran", cover: "https://upload.wikimedia.org/wikipedia/commons/7/70/PUT_IT_ALL_ON_ME.jpg" }
+      
     ]
   };
 
@@ -15,6 +29,7 @@ const mockPlaylists = {
 
   btn.addEventListener('click', () => {
     const mood = input.value.trim().toLowerCase();
+    console.log(mood)
     container.innerHTML = '';
     if (!mood || !mockPlaylists[mood]) {
       container.innerHTML = '<p style="color:#bbb;">Please enter a valid mood like "happy" or "chill".</p>';
